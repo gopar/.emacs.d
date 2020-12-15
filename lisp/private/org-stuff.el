f50fa2f111a6d4b0b9b36b53db1bf39b20d8ce40
Daniel Gopar <dg@sourcetheorem.com>
2020-12-14 20:23:58 -0800

    Updates

--- lisp/private/org-stuff.el
+++
@@ -1,8 +0,0 @@
-(setq org-link-abbrev-alist '(("SASB" . "https://datatheorem.atlassian.net/browse/SASB-%s")
-                              ("POR" . "https://datatheorem.atlassian.net/browse/POR-%s")
-                              ("CS" . "https://datatheorem.atlassian.net/browse/CS-%s")
-                              ("CUSTSUP" . "https://datatheorem.atlassian.net/browse/CUSTSUP-%s")
-                              ("API" . "https://datatheorem.atlassian.net/browse/API-%s")
-                              ("OPS" . "https://datatheorem.atlassian.net/browse/OPS-%s")))
-
-(provide 'org-stuff)
