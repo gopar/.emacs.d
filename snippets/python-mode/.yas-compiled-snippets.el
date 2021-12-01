7e1c8f4222e8278b4d0886a29162a846017b86f1
Daniel Gopar <dgopar@styleseat.com>
2021-12-01 07:08:16 -0800

    Moved snippets dir and some small changes

---
+++ snippets/python-mode/.yas-compiled-snippets.el
@@ -0,0 +1,10 @@
+;;; Compiled snippets and support files for `python-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'python-mode
+                     '(("t" "# TODO(Gopar): $0" "todo" nil nil nil "/Users/danielgopar/.emacs.d/snippets/python-mode/todo.yasnippet" nil nil)
+                       ("pudb" "import pudb;pudb.set_trace()$0\n" "pudb" nil nil nil "/Users/danielgopar/.emacs.d/snippets/python-mode/pudb.yasnippet" nil nil)
+                       ("meta" "class Meta:\n    $0" "Creates Meta class" nil nil nil "/Users/danielgopar/.emacs.d/snippets/python-mode/meta.yasnippet" nil nil)))
+
+
+;;; Do not edit! File generated at Fri Nov 26 13:38:18 2021
