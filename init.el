d0798f00259b0c2b0b5574cf69cd3bd3e38f959e
Daniel Gopar <dgopar@styleseat.com>
2022-07-14 11:15:10 -0500

    Changes

--- init.el
+++ init.el
@@ -10,7 +10,8 @@
         package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                            ("nongnu" . "https://elpa.nongnu.org/nongnu/")
                            ("melpa" . "https://melpa.org/packages/")
-                           ("org" . "https://orgmode.org/elpa/")))
+                           ;; ("org" . "https://orgmode.org/elpa/")
+                           ))
   (setq gnutls-algorithm-priority "normal:-vers-tls1.3")
   (package-initialize)
 
