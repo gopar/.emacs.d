78b8ad66faa978573bffea0bdbcb4d24a0f9be47
Daniel Gopar <dgopar@styleseat.com>
2022-08-18 11:52:44 -0500

    moving to new laptop

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (let ((default-directory user-emacs-directory)
       ;; (file-name-handler-alist nil)
-      ;; (gc-cons-percentage .9)
-      ;; (gc-cons-threshold most-positive-fixnum)
+      (gc-cons-percentage .9)
+      (gc-cons-threshold most-positive-fixnum)
       (read-process-output-max (* 1024 1024)))
 
   (require 'package)
@@ -47,3 +47,15 @@
   (setq inhibit-startup-echo-area-message "gopar")
   (org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
   (garbage-collect))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
