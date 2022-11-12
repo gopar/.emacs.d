e0e7a52a7cf211610f7bb65ecb0fc8fa62dee23c
Gopar <gopardaniel@gmail.com>
2022-11-11 22:09:11 -0600

    Added some new org config and straight.el

--- early-init.el
+++ early-init.el
@@ -9,7 +9,24 @@
             (lambda ()
               (setq gc-cons-percentage default-gc-percentage
                     gc-cons-threshold default-gc-threshold))))
+
+;;; straight.el bootstrap
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+
+
 ;; No package.el (for emacs 27 and later).  See
 ;; https://git.savannah.gnu.org/cgit/emacs.git/commit/?id=24acb31c04b4048b85311d794e600ecd7ce60d3b
-;; (setq package-enable-at-startup nil)
+(setq package-enable-at-startup nil)
 (setq load-prefer-newer t)
