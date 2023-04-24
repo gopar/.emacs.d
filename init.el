91e381f66abf918243496ecaaa5fe76fd676b266
Gopar <gopardaniel@gmail.com>
2023-04-24 09:51:21 -0500

    home

--- init.el
+++ init.el
@@ -1,52 +1,98 @@
-(let ((default-directory user-emacs-directory)
-      ;; (file-name-handler-alist nil)
-      (gc-cons-percentage .9)
-      (gc-cons-threshold most-positive-fixnum)
-      (read-process-output-max (* 1024 1024)))
+(require 'package)
 
-  (require 'package)
+(setq package-enable-at-startup nil
+      package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ;; ("org" . "https://orgmode.org/elpa/")
+                         ))
+(setq gnutls-algorithm-priority "normal:-vers-tls1.3")
+(package-initialize)
 
-  (setq package-enable-at-startup nil
-        package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                           ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-                           ("melpa" . "https://melpa.org/packages/")
-                           ;; ("org" . "https://orgmode.org/elpa/")
-                           ))
-  (setq gnutls-algorithm-priority "normal:-vers-tls1.3")
-  (package-initialize)
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;; Path to nano emacs modules (mandatory)
+;; (add-to-list 'load-path "/Users/gopar/personal/nano-emacs")
 
-  ;; Bootstrap `use-package'
-  (unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package))
+;; ;; Default layout (optional)
+;; (require 'nano-layout)
 
-  (setq use-package-always-ensure nil)
+;; ;; Theming Command line options (this will cancel warning messages)
+;; (add-to-list 'command-switch-alist '("-dark"   . (lambda (args))))
+;; (add-to-list 'command-switch-alist '("-light"  . (lambda (args))))
+;; (add-to-list 'command-switch-alist '("-default"  . (lambda (args))))
+;; (add-to-list 'command-switch-alist '("-no-splash" . (lambda (args))))
+;; (add-to-list 'command-switch-alist '("-no-help" . (lambda (args))))
+;; (add-to-list 'command-switch-alist '("-compact" . (lambda (args))))
 
-  ;; For some reason shr is **SOMETIMES** being loaded when going through `org-babel-load-file'???
-  ;; Sigh yet another thing I don't understand. Setting this here to avoid headaches
-  ;; Update: Might be because of byte compiling the org doc. Had a similar issue. Investigate
-  (setq shr-map
-        (let ((map (make-sparse-keymap)))
-          (define-key map "a" 'shr-show-alt-text)
-          ;; (define-key map "i" 'shr-browse-image)
-          (define-key map "z" 'shr-zoom-image)
-          ;; (define-key map [?\t] 'shr-next-link)
-          ;; (define-key map [?\M-\t] 'shr-previous-link)
-          (define-key map [follow-link] 'mouse-face)
-          (define-key map [mouse-2] 'shr-browse-url)
-          (define-key map [C-down-mouse-1] 'shr-mouse-browse-url-new-window)
-          ;; (define-key map "I" 'shr-insert-image)
-          ;; (define-key map "w" 'shr-maybe-probe-and-copy-url) ;; Might keep this one
-          ;; (define-key map "u" 'shr-maybe-probe-and-copy-url) ;; Might keep this one
-          ;; (define-key map "v" 'shr-browse-url)
-          ;; (define-key map "O" 'shr-save-contents)
-          (define-key map (kbd "RET") 'shr-browse-url)
-          map))
 
-  ;; Have to set it up here, otherwise it won't take effect in org mode doc
-  (setq inhibit-startup-echo-area-message "gopar")
-  (org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
-  (garbage-collect))
+;; ;; Customize support for 'emacs -q' (Optional)
+;; ;; You can enable customizations by creating the nano-custom.el file
+;; ;; with e.g. `touch nano-custom.el` in the folder containing this file.
+;; (let* ((this-file  (or load-file-name (buffer-file-name)))
+;;        (this-dir  (file-name-directory this-file))
+;;        (custom-path  (concat this-dir "nano-custom.el")))
+;;   (when (and (eq nil user-init-file)
+;;              (eq nil custom-file)
+;;              (file-exists-p custom-path))
+;;     (setq user-init-file this-file)
+;;     (setq custom-file custom-path)
+;;     (load custom-file)))
+
+;; ;; Theme
+;; (require 'nano-faces)
+;; (require 'nano-theme)
+;; (require 'nano-theme-dark)
+;; (require 'nano-theme-light)
+
+;; (nano-theme-set-dark)
+;; (call-interactively 'nano-refresh-theme)
+
+;; ;; Nano default settings (optional)
+;; (require 'nano-defaults)
+
+;; ;; Nano session saving (optional)
+;; (require 'nano-session)
+
+;; ;; Nano header & mode lines (optional)
+;; (require 'nano-modeline)
+
+;; ;; Nano key bindings modification (optional)
+;; (require 'nano-bindings)
+
+;; ;; Compact layout (need to be loaded after nano-modeline)
+;; (when (member "-compact" command-line-args)
+;;   (require 'nano-compact))
+
+;; ;; Nano counsel configuration (optional)
+;; ;; Needs "counsel" package to be installed (M-x: package-install)
+;; ;; (require 'nano-counsel)
+
+;; ;; Welcome message (optional)
+;; (let ((inhibit-message t))
+;;   (message "Welcome to GNU Emacs / N Λ N O edition")
+;;   (message (format "Initialization time: %s" (emacs-init-time))))
+
+;; ;; Splash (optional)
+;; (unless (member "-no-splash" command-line-args)
+;;   (require 'nano-splash))
+
+;; ;; Help (optional)
+;; (unless (member "-no-help" command-line-args)
+;;   (require 'nano-help))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(setq use-package-always-ensure nil)
+
+;; Have to set it up here, otherwise it won't take effect in org mode doc
+(setq inhibit-startup-echo-area-message "gopar")
+(org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
