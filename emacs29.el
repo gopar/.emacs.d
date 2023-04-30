3c010298ba5e7346b88016325789aa1e3cb9b00b
Gopar <gopardaniel@gmail.com>
2023-04-30 11:08:50 -0500

    Playing with neotree and treemacs

--- emacs29.el
+++ emacs29.el
@@ -211,12 +211,17 @@ next potential sentence end"
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defvar gopar-pair-programming nil)
 (defun gopar/pair-programming ()
-  "Poor mans minor mode for setting up things that i like to make pair programmikng easier."
+  "Poor mans minor mode for setting up things that i like to make pair programming easier."
   (interactive)
   (if gopar-pair-programming
       (progn
-        ;; dont display line numbers
-        (global-display-line-numbers-mode -1)
+        ;; Don't use global line numbers mode since it will turn on in other modes that arent programming
+        (dolist (buffer (buffer-list))
+          (with-current-buffer buffer
+            (when (derived-mode-p 'prog-mode)
+              (display-line-numbers-mode -1))))
+        (remove-hook 'prog-mode-hook 'display-line-numbers-mode)
+
         ;; disable all themes change to a friendlier theme
         (mapcar 'disable-theme custom-enabled-themes)
         ;; (load-theme 'tao-yin)
@@ -224,11 +229,21 @@ next potential sentence end"
 
     (progn
       ;; display line numbers
-      (global-display-line-numbers-mode)
+      (dolist (buffer (buffer-list))
+        (with-current-buffer buffer
+          (when (derived-mode-p 'prog-mode)
+            (display-line-numbers-mode 1))))
+      (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
       ;; disable all themes change to a friendlier theme
       (mapcar 'disable-theme custom-enabled-themes)
       (load-theme 'manoj-dark)
       (setq gopar-pair-programming t))))
+
+(use-package display-line-numbers
+  :ensure nil
+  ;; :hook (display-line-numbers-mode . gopar/display-line-numbers-mode-blacklist)
+  )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package projectile
@@ -860,11 +875,11 @@ Stolen from aweshell."
   :custom
   (dired-listing-switches "-alhoF --group-directories-first"))
 
-(use-package all-the-icons-dired
-  :ensure t
-  :hook (dired-mode . all-the-icons-dired-mode)
-  :custom
-  (all-the-icons-dired-monochrome nil))
+;; (use-package all-the-icons-dired
+;;   :ensure t
+;;   :hook (dired-mode . all-the-icons-dired-mode)
+;;   :custom
+;;   (all-the-icons-dired-monochrome nil))
 
 (use-package files
   :ensure nil
@@ -1063,14 +1078,116 @@ go through the search engine"
 
 (use-package neotree
   :ensure t
-  :bind ("<f5>" . neotree-toggle
-         )
+  :bind ("<f5>" . neotree-toggle)
   :custom
   (neo-theme 'icons)
   (neo-smart-open t)
+  (neo-autorefresh t)
   ;; takes too long to update on first try
   ;; (neo-vc-integration '(face char))
   (neo-show-hidden-files t))
+
+;; (use-package treemacs
+;;   :ensure t
+;;   :defer t
+;;   ;; :init
+;;   ;; (with-eval-after-load 'winum
+;;   ;;   (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+;;   :config
+;;   (progn
+;;     (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
+;;           treemacs-deferred-git-apply-delay        0.5
+;;           treemacs-directory-name-transformer      #'identity
+;;           treemacs-display-in-side-window          t
+;;           treemacs-eldoc-display                   'detailed ;test
+;;           treemacs-file-event-delay                2000
+;;           treemacs-file-extension-regex            treemacs-last-period-regex-value
+;;           treemacs-file-follow-delay               0.2
+;;           treemacs-file-name-transformer           #'identity
+;;           treemacs-follow-after-init               t
+;;           treemacs-expand-after-init               t
+;;           treemacs-find-workspace-method           'find-for-file-or-pick-first
+;;           treemacs-git-command-pipe                ""
+;;           treemacs-goto-tag-strategy               'refetch-index
+;;           treemacs-header-scroll-indicators        '(nil . "^^^^^^")
+;;           treemacs-hide-dot-git-directory          nil
+;;           treemacs-indentation                     2
+;;           treemacs-indentation-string              " "
+;;           treemacs-is-never-other-window           nil
+;;           treemacs-max-git-entries                 5000
+;;           treemacs-missing-project-action          'ask
+;;           treemacs-move-forward-on-expand          nil
+;;           treemacs-no-png-images                   nil
+;;           treemacs-no-delete-other-windows         t
+;;           treemacs-project-follow-cleanup          nil
+;;           treemacs-persist-file                    (expand-file-name "var/treemacs/persist.org" user-emacs-directory)
+;;           treemacs-position                        'left
+;;           treemacs-read-string-input               'from-child-frame
+;;           treemacs-recenter-distance               0.1
+;;           treemacs-recenter-after-file-follow      nil
+;;           treemacs-recenter-after-tag-follow       nil
+;;           treemacs-recenter-after-project-jump     'always
+;;           treemacs-recenter-after-project-expand   'on-distance
+;;           treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+;;           treemacs-project-follow-into-home        nil
+;;           treemacs-show-cursor                     nil
+;;           treemacs-show-hidden-files               t
+;;           treemacs-silent-filewatch                nil
+;;           treemacs-silent-refresh                  nil
+;;           treemacs-sorting                         'alphabetic-asc
+;;           treemacs-select-when-already-in-treemacs 'move-back
+;;           treemacs-space-between-root-nodes        t
+;;           treemacs-tag-follow-cleanup              t
+;;           treemacs-tag-follow-delay                1.5
+;;           treemacs-text-scale                      nil
+;;           treemacs-user-mode-line-format           nil
+;;           treemacs-user-header-line-format         nil
+;;           treemacs-wide-toggle-width               70
+;;           treemacs-width                           35
+;;           treemacs-width-increment                 1
+;;           treemacs-width-is-initially-locked       t
+;;           treemacs-workspace-switch-cleanup        nil)
+
+;;     ;; The default width and height of the icons is 22 pixels. If you are
+;;     ;; using a Hi-DPI display, uncomment this to double the icon size.
+;;     ;;(treemacs-resize-icons 44)
+
+;;     (treemacs-follow-mode t)
+;;     (treemacs-filewatch-mode t)
+;;     (treemacs-fringe-indicator-mode 'always)
+;;     (when treemacs-python-executable
+;;       (treemacs-git-commit-diff-mode t))
+
+;;     (pcase (cons (not (null (executable-find "git")))
+;;                  (not (null treemacs-python-executable)))
+;;       (`(t . t)
+;;        (treemacs-git-mode 'deferred))
+;;       (`(t . _)
+;;        (treemacs-git-mode 'simple)))
+
+;;     (treemacs-hide-gitignored-files-mode nil))
+;;   ;; :bind
+;;   ;; (:map global-map
+;;   ;;       ("M-0"       . treemacs-select-window)
+;;   ;;       ("C-x t 1"   . treemacs-delete-other-windows)
+;;   ;;       ("C-x t t"   . treemacs)
+;;   ;;       ("C-x t d"   . treemacs-select-directory)
+;;   ;;       ("C-x t B"   . treemacs-bookmark)
+;;   ;;       ("C-x t C-t" . treemacs-find-file)
+;;   ;;       ("C-x t M-t" . treemacs-find-tag))
+;;   )
+
+;; (use-package treemacs-projectile
+;;   :after (treemacs projectile)
+;;   :ensure t)
+
+;; (use-package treemacs-icons-dired
+;;   :hook (dired-mode . treemacs-icons-dired-enable-once)
+;;   :ensure t)
+
+;; (use-package treemacs-magit
+;;   :after (treemacs magit)
+;;   :ensure t)
 
 (use-package dizzee
   :ensure t
