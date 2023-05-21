726f090ccfc4498774134127b6e4bd930c03bda8
Gopar <gopardaniel@gmail.com>
2023-05-20 21:41:45 -0500

    yea

--- nano-ignoreme.el
+++ nano-ignoreme.el
@@ -87,8 +87,12 @@
       (null-device . "/dev/null"))
      (eshell-connection-default-profile
       (eshell-path-env-list))))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages
-   '(ace-window helpful iedit consult-ag dashboard magit-todos org-edna org-pomodoro dockerfile-mode yasnippet virtualenvwrapper which-key typescript-mode visual-fill-column eshell-git-prompt corfu compact-docstrings string-inflection tree-sitter-langs dizzee expand-region marginalia projectile yaml-mode git-gutter rainbow-mode chatgpt-shell dumb-jump flycheck corral avy markdown-mode dash-at-point all-the-icons-dired neotree vertico-posframe ledger-mode hydra eshell-vterm highlight-indentation orderless fancy-compilation vue-mode consult-org-roam pdf-tools dall-e-shell all-the-icons-ibuffer no-littering cape code-compass move-text eshell-syntax-highlighting ibuffer-vc all-the-icons-completion org-present)))
+   '(shell-maker ace-window helpful iedit consult-ag dashboard magit-todos org-edna org-pomodoro dockerfile-mode yasnippet virtualenvwrapper which-key typescript-mode visual-fill-column eshell-git-prompt corfu compact-docstrings string-inflection tree-sitter-langs dizzee expand-region marginalia projectile yaml-mode git-gutter rainbow-mode chatgpt-shell dumb-jump flycheck corral avy markdown-mode dash-at-point all-the-icons-dired neotree vertico-posframe ledger-mode hydra eshell-vterm highlight-indentation orderless fancy-compilation vue-mode consult-org-roam pdf-tools dall-e-shell all-the-icons-ibuffer no-littering cape code-compass move-text eshell-syntax-highlighting ibuffer-vc all-the-icons-completion org-present))
+ '(safe-local-variable-values
+   '((eval setenv "DJANGO_SETTINGS_MODULE" "config.settings.dev")
+     (eval venv-workon "env"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
