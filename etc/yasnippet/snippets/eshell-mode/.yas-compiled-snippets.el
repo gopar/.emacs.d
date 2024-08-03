7464f27e73fcbd74a9b3e2927709620a9fb6d88f
Gopar <gopardaniel@gmail.com>
2024-08-03 14:24:05 -0700

    Updates to Org Roam, Eshell and Chatgpt

---
+++ etc/yasnippet/snippets/eshell-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `eshell-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'eshell-mode
+                     '(("for" "for f in ${1:*} { ${2:echo} \"$f\"; $3} $0" "Eshell For Loop" nil nil nil "/Users/gopar/.emacs.d/etc/yasnippet/snippets/eshell-mode/for" nil nil)))
+
+
+;;; Do not edit! File generated at Sat Aug  3 14:12:38 2024
