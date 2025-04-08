d73924b05aaaa14ccaa571b1e36cfdc3deb67eeb
Gopar <gopardaniel@gmail.com>
2025-04-08 12:03:44 -0700

    Refactor setq to setopt and add org modern packages

--- init.el
+++ init.el
@@ -1,24 +1,29 @@
-(require 'package)
-(setq use-package-verbose t)
-(setq use-package-always-ensure nil)
+(setopt use-package-verbose t
+        use-package-always-ensure nil)
 
 (use-package gcmh
   :ensure t
   :demand t)
 
-(setq package-enable-at-startup nil
-      package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-                         ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-                         ("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")))
-(package-initialize)
+(use-package package
+  :ensure nil
+  :defer t
+  :commands (list-packages package-install)
+  :custom
+  (package-enable-at-startup nil)
+  (package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                      ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+                      ("melpa" . "https://melpa.org/packages/")
+                      ("org" . "https://orgmode.org/elpa/")))
+  :config
+  (package-initialize))
 
 ;; Have to set it up here, otherwise it won't take effect in org mode doc
-(setq inhibit-startup-echo-area-message user-login-name)
+(setopt inhibit-startup-echo-area-message user-login-name)
 
 (load (locate-user-emacs-file "README.el"))
 
 ;; Mac only
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 (add-to-list 'default-frame-alist '(ns-appearance . dark))
-(setq ns-use-proxy-icon nil)
+(setopt ns-use-proxy-icon nil)
