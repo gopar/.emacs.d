4993a3c6cfdffb66ec30fce5d286868a8559c820
Gopar <gopardaniel@gmail.com>
2025-04-07 17:49:13 -0700

    Mainly a whole lot of speed improvements

--- init.el
+++ init.el
@@ -1,20 +1,20 @@
 (require 'package)
+(setq use-package-verbose t)
+(setq use-package-always-ensure nil)
 
 (use-package gcmh
   :ensure t
-  :load t)
+  :demand t)
 
 (setq package-enable-at-startup nil
       package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("nongnu" . "https://elpa.nongnu.org/nongnu/")
                          ("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")))
-;; (setq gnutls-algorithm-priority "normal:-vers-tls1.3")
 (package-initialize)
 
 ;; Have to set it up here, otherwise it won't take effect in org mode doc
 (setq inhibit-startup-echo-area-message user-login-name)
-(setq use-package-always-ensure nil)
 
 (load (locate-user-emacs-file "README.el"))
 
